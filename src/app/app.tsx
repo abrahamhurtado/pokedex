69dbcf88d662ae815363b09cb0f727f4839a41a1
Abraham Hurtado <arhc.hc@gmail.com>
2023-03-30 23:35:35 -0700

    Initial commit

---
+++ src/app/app.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+import NxWelcome from './nx-welcome';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <NxWelcome title="pokedex-2023" />
+    </StyledApp>
+  );
+}
+
+export default App;
