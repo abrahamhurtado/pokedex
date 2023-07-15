af5905bed76a883e7a28645168e4598a7ee1484f
Abraham Hurtado <arhc.hc@gmail.com>
2023-07-15 00:03:05 -0700

    Pokemon typings theming

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,4 +2,6 @@ import { IPokemon } from "pokeapi-typescript";
 
 export interface PokemonMap {
   [name: string]: IPokemon
-}
+};
+
+export type PokemonTypes = 'fire' | 'water' | 'dark' | 'psychic' | 'grass' | 'poison' | 'fairy' | 'electric' | 'dragon' | 'fighting' | 'normal' | 'ghost' | 'steel' | 'flying' | 'rock' | 'ground' | 'ice' | 'bug'
